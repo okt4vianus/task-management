89e9cb8f1df4c1db366cf42fd0b0b53e9607565e
M Haidar Hanif <git@mhaidarhanif.com>
2025-01-10 20:29:05 +0700

    chore: tidy up

--- src/App.tsx
+++ src/App.tsx
@@ -1,44 +1,16 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
-import "./App.css";
-import { add } from "./math.ts";
+import { add } from "./lib/math.ts";
 
-function App() {
-  const [count, setCount] = useState(0);
-
-  const bilangan1 = 2;
-  const bilangan2 = 3;
-  const tambah = add(bilangan1, bilangan2);
+export function App() {
+  const personAge1 = 20;
+  const personAge2 = 30;
+  const personAgeSum = add(personAge1, personAge2);
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <h2>Penjumlahan 2 bilangan</h2>
+    <div>
+      <h2>Sum of 2 numbers</h2>
       <h2>
-        {bilangan1} + {bilangan2} = {tambah}
+        {personAge1} + {personAge2} = {personAgeSum}
       </h2>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    </div>
   );
 }
-
-export default App;
