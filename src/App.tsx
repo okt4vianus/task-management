7fb4ed0c94eed403596f485bb98373e8d62454e5
Oktavianus R. Tasak <okt4vianus+git@gmail.com>
2025-01-20 21:02:54 +0800

    useState

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 // import { TaskList } from "./components/task-list";
 // import { Task } from "./types/task";
+import { AddButton } from "./components/button";
 import { TaskListCmp } from "./components/task-list-cmp";
 
 export function App() {
@@ -31,14 +32,19 @@ export function App() {
   ];
 
   return (
-    <div className="flex justify-center p-5">
-      <div className="w-full max-w-lg space-y-5">
-        <header>
-          <h1 className="text-center text-3xl font-bold text-blue-700 underline">
-            Task Management
-          </h1>
-        </header>
-        <TaskListCmp tasks={tasks} />
+    <div>
+      <div className="flex justify-center p-5">
+        <div className="w-full max-w-lg space-y-5">
+          <header>
+            <h1 className="text-center text-3xl font-bold text-blue-700 underline">
+              Task Management
+            </h1>
+          </header>
+          <TaskListCmp tasks={tasks} />
+        </div>
+      </div>
+      <div className="flex justify-center text-xl">
+        <AddButton />
       </div>
     </div>
   );
---
+++ src/components/button.tsx
@@ -0,0 +1,42 @@
+import { useState } from "react";
+
+export function AddButton() {
+  const [count, setCount] = useState(0);
+  const [name, setName] = useState("0");
+
+  function handleIncrement() {
+    setCount(count + 1);
+
+    console.log(count);
+
+    setName(count);
+  }
+
+  function handleDecrease() {
+    setCount(count - 1);
+
+    console.log(count);
+
+    setName(count);
+  }
+
+  return (
+    <div>
+      <button
+        onClick={handleIncrement}
+        className="rounded bg-blue-700 text-white"
+      >
+        + add
+      </button>
+      <span> </span>
+      <button
+        onClick={handleDecrease}
+        className="rounded bg-red-700 text-white"
+      >
+        - sub
+      </button>
+      <p>Counter: {count}</p>
+      <p>{name}</p>
+    </div>
+  );
+}
